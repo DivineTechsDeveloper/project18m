46498da96cad65ad02e09545ec635898ce86dbb3
ganeshbabooo <babufamily@gmail.com>
2021-09-24 20:30:12 +0530

    app folder

---
+++ app/src/main/java/com/salezman/quizapp/Util/Constant.java
@@ -0,0 +1,9 @@
+package com.salezman.quizapp.Util;
+
+public class Constant {
+
+    public static String PURCHASED_CODE = "cc2eb058-e617-406f-b2b9-fe392a6981d3";
+
+    public static String cat_id;
+
+}
